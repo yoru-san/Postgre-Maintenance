43ede264941e0b6bbcca22a5817183b9667484cb
AzarielUr <fa.corneau@gmail.com>
2018-02-15 14:19:39 -0600

    save and conf

---
+++ conf.sh
@@ -0,0 +1,5 @@
+USERNAME="postgres"
+HOST="localhost"
+DBLIST=
+DIR="$HOME/db/save"
+
--- save_db.sh
+++ save_db.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
 
-dir=$HOME/db/save
+source ./conf.sh
+
+
 date=`date +"%Y_%m_%d"`
-DBNAMES="`psql -U appli_web appli_web -l -A -F: | sed -ne "/:/ { /Name:Owner/d; /template1/d; /template0/d; s/:.*$//; p }"`"
+
+if [ "$HOST" = "localhost" ]; then
+	HOST="$USERNAME"
+	echo "---Backup for local server---"
+else
+	echo "---Backup for remote server : $HOST---"
+fi
+
+echo "postgre user : $USERNAME"
+
+DBNAMES="`psql -U $USERNAME $HOST -l -A -F: | sed -ne "/:/ { /Name:Owner/d; /template1/d; /template0/d; s/:.*$//; p }"`"
 for db in $DBNAMES 
 do
-	echo $db
-	mkdir -p $dir/$db
-	pg_dump -Fc $db > $dir/$db/$date.dump
+	filename="$DIR/$date.dump"
+
+	echo "Creating backup for $db ..."
+	if ! mkdir -p $DIR/$db; then
+		echo "ERROR : Failed to create file $dir/$db, check your prilileges"
+	fi
+	if ! pg_dump -Fc $db > $filename; then
+		echo "ERROR : Failed to dump database $db"
+	else
+		echo "Backup created for $db! --> $filename"
+	fi
 done
+
+echo "---Database dump finished---"
